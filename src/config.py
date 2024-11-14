c95fd543418be43a063d7b89063b7ab7d0709dfa
Jinsoo Yoon <floreo1242@gmail.com>
2024-11-15 04:14:35 +0900

    Fix: Set to default port

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = True
 HOST = os.environ.get('HOST','localhost')
-PORT = int(os.environ.get('PORT', '5005'))
+PORT = int(os.environ.get('PORT', '5000'))
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
